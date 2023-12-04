cce5fe2efcd6f0330f5606dab99c99164dd7d9de
Tam Tran <trancotam@gmail.com>
2023-12-04 17:46:25 +0700

    add code
    Signed-off-by: Tam Tran <trancotam@gmail.com>

--- test.py
+++ test.py
@@ -50,24 +50,72 @@ def exportCompanyOfficers (symbol='HPG'):
     with pd.ExcelWriter(file_companyOverview_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
         sheet_name = "financialRatio"
         financialRatio.to_excel(writer, sheet_name=sheet_name, index=True)
-    
-def main():
-    print("Chương trình bắt đầu.")
-    
-    
-    # exportListCompanies()
-    # exportcompany_overview(ticket="HPG")
-    # exportCompanyOfficers('HPG')
-    # exportCompanyOfficers('HPG')
+
+def exportFinancialRatioCompare (symbol='HPG'):
     financialRatioCompare = financial_ratio_compare (symbol_ls=["CTG", "TCB", "ACB"], industry_comparison=True, frequency='Yearly', start_year=2010)
-    print(">>>financialRatioCompare :\n",financialRatioCompare)
     with pd.ExcelWriter(file_companyOverview_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
         sheet_name = "financialRatioCompare"
         financialRatioCompare.to_excel(writer, sheet_name=sheet_name, index=True)
-    print("Chương trình kết thúc.")
 
+def exportFinancialReportIncomeStatement (symbol='HPG'):
+    financialReport = financial_report (symbol=symbol, report_type='IncomeStatement', frequency='Quarterly')
+    with pd.ExcelWriter(file_companyOverview_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+        sheet_name = "Bao_cao_doanh_thu"
+        financialReport.to_excel(writer, sheet_name=sheet_name, index=True)
+
+def exportFinancialReportBalanceSheet (symbol='HPG'):
+    financialReport = financial_report (symbol=symbol, report_type='BalanceSheet', frequency='Quarterly')
+    with pd.ExcelWriter(file_companyOverview_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+        sheet_name = "Bang_can_doi_tien_te"
+        financialReport.to_excel(writer, sheet_name=sheet_name, index=True)
+
+def exportFinancialReportCashFlow (symbol='HPG'):
+    financialReport = financial_report (symbol='SSI', report_type='CashFlow', frequency='Quarterly')
+    with pd.ExcelWriter(file_companyOverview_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+        sheet_name = "Bang_luu_chuyen_tien_te"
+        financialReport.to_excel(writer, sheet_name=sheet_name, index=True)
+
+def exportStockEvaluation (symbol='HPG'):
+    stockEvaluation = stock_evaluation (symbol=symbol, period=1, time_window='D')
+    print(">>>stockEvaluation :\n",stockEvaluation);
+    with pd.ExcelWriter(file_companyOverview_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+        sheet_name = "stockEvaluation"
+        stockEvaluation.to_excel(writer, sheet_name=sheet_name, index=True)
+
+def main():
+    print("Chương trình bắt đầu.")
+    symbol='HPG'
     
+    # exportListCompanies()
+    # exportcompany_overview(symbol)
+    # exportCompanyOfficers(symbol)
+    # exportCompanyOfficers(symbol)
+    # exportFinancialRatioCompare(symbol)
 
+    # # Báo cáo tài chính
+    # # Income Statement: Báo cáo doanh thu
+    # exportFinancialReportIncomeStatement(symbol)
+    # # Balance Sheet: Bảng cân đối kế toán
+    # exportFinancialReportBalanceSheet(symbol)
+    # # Cashflow: Báo cáo lưu chuyển tiền tệ
+    # exportFinancialReportCashFlow(symbol)
+    
+    # exportStockEvaluation (symbol='HPG')
+
+    df = stock_historical_data(symbol='VNINDEX', start_date='2022-01-01', end_date='2023-10-10', resolution='1D', type='index')
+    print(">>>DEBUG df",type(df))
+    fig = candlestick_chart(df, 
+                    title='VNINDEX Candlestick Chart with MA and Volume', x_label='Date', y_label='Price', ma_periods=[50,200], 
+                    show_volume=True, figure_size=(15, 8), reference_period=300, 
+                    colors=('lightgray', 'gray'), reference_colors=('black', 'blue'))
+    fig.show()
+    fig.write_image("VNINDEX_candlestick.png")
+
+
+
+    price_board('TCB,SSI,VND')
+
+    print("Chương trình kết thúc.")
 
 if __name__ == "__main__":
     main()
